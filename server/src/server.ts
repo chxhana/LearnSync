a9d7e8d8c3f453dc36e375fc2488f49aeb5c7535
Chahana Dahal <chahana@chahanas-mbp.devices.wcusers.int>
2024-03-18 13:24:52 -0600

    Added pie chart and bar graph.

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,9 +8,7 @@ const app = express();
 const port = 3001;
 app.use(cors())
 
-const params = {
-    enrollment_type: 'teacher',
-  };
+
   
   const headers = {
     Authorization: 'Bearer ' + ACCESS_TOKEN,
@@ -20,7 +18,12 @@ const params = {
     res.send("This is home page.")
   })
 
+  //shows all courses for the teacher
+
   app.get('/api/courses', async(req, res)=>{
+    const params = {
+        enrollment_type: 'teacher',
+      };
       try{
           const response = await axios.get(`https://westminster.instructure.com/api/v1/courses`, {params, headers})
           res.json(response.data)
@@ -31,12 +34,14 @@ const params = {
 
   });
 
+  //shows all students in the courses
+
   app.get('/api/courses/:course_id/students', async (req, res) => {
     const courseId = req.params.course_id;
 
     try {
         const response = await axios.get(`https://westminster.instructure.com/api/v1/courses/${courseId}/students`, {
-            params,
+            
             headers
         });
         res.json(response.data);
@@ -46,28 +51,16 @@ const params = {
     }
 });
 
-    app.get('/api/v1/users/:id', async(req, res) => {
-        const userId = req.params.id;
 
-        try {
-            const response = await axios.get(`https://westminster.instructure.com/api/v1/`, {
-                params,
-                headers
-            });
-            res.json(response.data);
-        } catch(error) {
-            console.error(error);
-            res.status(500).json({ error: 'Internal server error' });
-        }
-    });
-   
+
+//stats for students    
 
   app.get('/api/courses/:course_id/analytics/student_summaries', async(req, res) =>{
     const courseId = req.params.course_id;
     
     try {
         const response = await axios.get(`https://westminster.instructure.com/api/v1/courses/${courseId}/analytics/student_summaries`, {
-            params,
+            
             headers
         });
         res.json(response.data);
@@ -76,13 +69,15 @@ const params = {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
+
+//shows all assignments for the course
 
   app.get('/api/courses/:course_id/assignments', async(req, res) =>{
     const courseId = req.params.course_id;
 
     try {
         const response = await axios.get(`https://westminster.instructure.com/api/v1/courses/${courseId}/assignments`, {
-            params,
+          
             headers
         });
         res.json(response.data);
@@ -92,17 +87,16 @@ const params = {
     }
 });
 
+//grades
 
-
-app.get('/api/courses/:course_id/assignments/:id/submissions', async(req, res) =>{
+app.get('/api/audit/grade_change/assignments/:course_id', async (req, res) => {
     const courseId = req.params.course_id;
-    const assignmentId = req.params.id;
-    
+    //const assignmentId = req.params.assignment_id;
+
 
     try {
-        const response = await axios.get(`https://westminster.instructure.com/api/v1/courses/${courseId}/assignments/${assignmentId}/submissions`, {
-            params,
-            headers
+        const response = await axios.get(`https://westminster.instructure.com/api/v1/audit/grade_change/assignments/${courseId}`, {
+            headers,
         });
         res.json(response.data);
     } catch(error) {
@@ -111,6 +105,52 @@ app.get('/api/courses/:course_id/assignments/:id/submissions', async(req, res) =
     }
 });
 
+// grades
+app.get('/api/courses/:course_id/students/submissions', async (req, res) => {
+    const courseId = req.params.course_id;
+
+    try {
+        const response = await axios.get(`https://westminster.instructure.com/api/v1/courses/${courseId}/students/submissions`, {
+            headers,
+        
+        });
+        res.json(response.data);
+    } catch(error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+//user id
+
+app.get('/api/courses/:course_id/enrollments' , async(req, res) =>{
+    const courseId = req.params.course_id;
+    try {
+        const response = await axios.get(`https://westminster.instructure.com/api/v1/courses/${courseId}/enrollments`, {
+            
+            headers
+        });
+        res.json(response.data);
+    } catch(error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+// quiz
+app.get('/api/courses/:course_id/quizzes', async (req, res) => {
+    const courseId = req.params.course_id;
+
+    try {
+        const response = await axios.get(`https://westminster.instructure.com/api/v1/courses/${courseId}/quizzes`, {
+            headers
+        });
+        res.json(response.data);
+    } catch(error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+//assignment name?
 app.get('/api/courses/:course_id/assignments/:id', async(req, res) =>{
     const courseId = req.params.course_id;
     const assignmentId = req.params.id;
@@ -118,7 +158,7 @@ app.get('/api/courses/:course_id/assignments/:id', async(req, res) =>{
 
     try {
         const response = await axios.get(`https://westminster.instructure.com/api/v1/courses/${courseId}/assignments/${assignmentId}`, {
-            params,
+            
             headers
         });
         res.json(response.data);
@@ -128,13 +168,17 @@ app.get('/api/courses/:course_id/assignments/:id', async(req, res) =>{
     }
 });
 
-app.get('/api/users/:user_id/courses/:course_id/assignments', async(req, res) =>{
+app.get('/api/courses/:course_id/gradebook_history/feed', async(req, res) =>{
     const courseId = req.params.course_id;
-    const studentId = req.params.user_id;
+    //const assignmentId = req.params.id;
+    
 
     try {
-        const response = await axios.get(`https://westminster.instructure.com/api/v1/users/${studentId}courses/${courseId}/assignments`, {
-            params,
+        const response = await axios.get(`https://westminster.instructure.com/api/v1/courses/${courseId}/gradebook_history/feed`, {
+            params :{
+                per_page: 1000,
+                grouped: true
+            },
             headers
         });
         res.json(response.data);
@@ -142,7 +186,11 @@ app.get('/api/users/:user_id/courses/:course_id/assignments', async(req, res) =>
         console.error(error);
         res.status(500).json({ error: 'Internal server error' });
     }
-}); //not working
+});
+
+
+
+
 
   app.listen(port, ()=> {
       console.log(`running on port: $(port)`)
---
+++ ui/src/components/common/piechart.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { PieChart, Pie, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+interface PieChartData {
+  name: string;
+  value: number;
+}
+
+interface Props {
+  data: PieChartData[];
+}
+
+const PieChartComponent: React.FC<Props> = ({ data }) => {
+  return (
+    <div>
+      <ResponsiveContainer width="100%" height={200}>
+        <PieChart>
+          <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={80} fill="#8884d8" label />
+          <Tooltip />
+          <Legend />
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default PieChartComponent;
